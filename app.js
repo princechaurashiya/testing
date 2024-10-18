a7f184288258b544c32d3963ed1dcbc81618944a
princechaurashiya <princechaurashiya163@gmail.com>
2024-10-18 17:37:30 +0530

    modify

--- app.js
+++ app.js
@@ -1,98 +1,203 @@
 
-const quizData = [{
-    question: "Which of the following is a client site language?",
-    a: "Java",
-    b: "C",
-    c: "Python",
-    d: "JavaScript",
-    correct: "d",
-},
-{
-    question: "What does HTML stand for?",
-    a: "Hypertext Markup Language",
-    b: "Cascading Style Sheet",
-    c: "Jason Object Notation",
-    d: "Helicopters Terminals Motorboats Lamborginis",
-    correct: "a",
-},
-{
-    question: "What year was JavaScript launched?",
-    a: "1996",
-    b: "1995",
-    c: "1994",
-    d: "none of the above",
-    correct: "b",
-},
-{
-    question: "What does CSS stands for?",
-    a: "Hypertext Markup Language",
-    b: "Cascading Style Sheet",
-    c: "Jason Object Notation",
-    d: "Helicopters Terminals Motorboats Lamborginis",
-    correct: "b",
-}
-];
-let index = 0;
-let correct = 0,
-incorrect = 0,
-total = quizData.length;
-let questionBox = document.getElementById("questionBox");
-let allInputs = document.querySelectorAll("input[type='radio']")
-const loadQuestion = () => {
-if (total === index) {
-    return quizEnd()
-}
-reset()
-const data = quizData[index]
-questionBox.innerHTML = `${index + 1}) ${data.question}`
-allInputs[0].nextElementSibling.innerText = data.a
-allInputs[1].nextElementSibling.innerText = data.b
-allInputs[2].nextElementSibling.innerText = data.c
-allInputs[3].nextElementSibling.innerText = data.d
-}
+// const quizData = [{
+//     question: "Which of the following is a client site language?",
+//     a: "Java",
+//     b: "C",
+//     c: "Python",
+//     d: "JavaScript",
+//     correct: "d",
+// },
+// {
+//     question: "What does HTML stand for?",
+//     a: "Hypertext Markup Language",
+//     b: "Cascading Style Sheet",
+//     c: "Jason Object Notation",
+//     d: "Helicopters Terminals Motorboats Lamborginis",
+//     correct: "a",
+// },
+// {
+//     question: "What year was JavaScript launched?",
+//     a: "1996",
+//     b: "1995",
+//     c: "1994",
+//     d: "none of the above",
+//     correct: "b",
+// },
+// {
+//     question: "What does CSS stands for?",
+//     a: "Hypertext Markup Language",
+//     b: "Cascading Style Sheet",
+//     c: "Jason Object Notation",
+//     d: "Helicopters Terminals Motorboats Lamborginis",
+//     correct: "b",
+// }
+// ];
+// let index = 0;
+// let correct = 0,
+// incorrect = 0,
+// total = quizData.length;
+// let questionBox = document.getElementById("questionBox");
+// let allInputs = document.querySelectorAll("input[type='radio']")
+// const loadQuestion = () => {
+// if (total === index) {
+//     return quizEnd()
+// }
+// reset()
+// const data = quizData[index]
+// questionBox.innerHTML = `${index + 1}) ${data.question}`
+// allInputs[0].nextElementSibling.innerText = data.a
+// allInputs[1].nextElementSibling.innerText = data.b
+// allInputs[2].nextElementSibling.innerText = data.c
+// allInputs[3].nextElementSibling.innerText = data.d
+// }
 
-document.querySelector("#submit").addEventListener(
-"click",
-function() {
-    const data = quizData[index]
-    const ans = getAnswer()
-    if (ans === data.correct) {
-        correct++;
-    } else {
-        incorrect++;
-    }
-    index++;
-    loadQuestion()
-}
-)
+// document.querySelector("#submit").addEventListener(
+// "click",
+// function() {
+//     const data = quizData[index]
+//     const ans = getAnswer()
+//     if (ans === data.correct) {
+//         correct++;
+//     } else {
+//         incorrect++;
+//     }
+//     index++;
+//     loadQuestion()
+// }
+// )
 
-const getAnswer = () => {
-let ans;
-allInputs.forEach(
-    (inputEl) => {
-        if (inputEl.checked) {
-            ans = inputEl.value;
+// const getAnswer = () => {
+// let ans;
+// allInputs.forEach(
+//     (inputEl) => {
+//         if (inputEl.checked) {
+//             ans = inputEl.value;
+//         }
+//     }
+// )
+// return ans;
+// }
+
+// const reset = () => {
+// allInputs.forEach(
+//     (inputEl) => {
+//         inputEl.checked = false;
+//     }
+// )
+// }
+
+// const quizEnd = () => {
+// // console.log(document.getElementsByClassName("container"));
+// document.getElementsByClassName("container")[0].innerHTML = `
+//     <div class="col">
+//          <h2> Thanks for participating. </h2>
+//         <h3 class="w-100">You've scored ${correct} / ${total} </h3>
+//     </div>
+// `
+// }
+// loadQuestion(index);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const quizData = [
+        {
+            question: "Which of the following is a client site language?",
+            a: "Java",
+            b: "C",
+            c: "Python",
+            d: "JavaScript",
+            correct: "d",
+        },
+        {
+            question: "What does HTML stand for?",
+            a: "Hypertext Markup Language",
+            b: "Cascading Style Sheet",
+            c: "Jason Object Notation",
+            d: "Helicopters Terminals Motorboats Lamborginis",
+            correct: "a",
+        },
+        {
+            question: "What year was JavaScript launched?",
+            a: "1996",
+            b: "1995",
+            c: "1994",
+            d: "none of the above",
+            correct: "b",
+        },
+        {
+            question: "What does CSS stands for?",
+            a: "Hypertext Markup Language",
+            b: "Cascading Style Sheet",
+            c: "Jason Object Notation",
+            d: "Helicopters Terminals Motorboats Lamborginis",
+            correct: "b",
         }
-    }
-)
-return ans;
-}
+    ];
 
-const reset = () => {
-allInputs.forEach(
-    (inputEl) => {
-        inputEl.checked = false;
-    }
-)
-}
+    let index = 0;
+    let correct = 0,
+        incorrect = 0,
+        total = quizData.length;
 
-const quizEnd = () => {
-// console.log(document.getElementsByClassName("container"));
-document.getElementsByClassName("container")[0].innerHTML = `
-    <div class="col">
-         <h2> Thanks for participating. </h2>
-        <h3 class="w-100">You've scored ${correct} / ${total} </h3>
-    </div>
-`
-}
-loadQuestion(index);
+    let questionBox = document.getElementById("questionBox");
+    let allInputs = document.querySelectorAll("input[type='radio']");
+
+    const loadQuestion = () => {
+        if (total === index) {
+            return quizEnd();
+        }
+        reset();
+        const data = quizData[index];
+        questionBox.innerHTML = `${index + 1}) ${data.question}`;
+        allInputs[0].nextElementSibling.innerText = data.a;
+        allInputs[1].nextElementSibling.innerText = data.b;
+        allInputs[2].nextElementSibling.innerText = data.c;
+        allInputs[3].nextElementSibling.innerText = data.d;
+    };
+
+    document.querySelector("#submit").addEventListener("click", function () {
+        const data = quizData[index];
+        const ans = getAnswer();
+
+        // Prevent advancing if no answer is selected
+        if (!ans) {
+            alert("Please select an answer!");
+            return;
+        }
+
+        if (ans === data.correct) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+        index++;
+        loadQuestion();
+    });
+
+    const getAnswer = () => {
+        let ans = null; // Initialize ans to null
+        allInputs.forEach((inputEl) => {
+            if (inputEl.checked) {
+                ans = inputEl.value;
+            }
+        });
+        return ans;
+    };
+
+    const reset = () => {
+        allInputs.forEach((inputEl) => {
+            inputEl.checked = false;
+        });
+    };
+
+    const quizEnd = () => {
+        document.getElementsByClassName("container")[0].innerHTML = `
+            <div class="col">
+                <h2>Thanks for participating.</h2>
+                <h3 class="w-100">You've scored ${correct} / ${total}</h3>
+            </div>
+        `;
+    };
+
+    loadQuestion(index);
+});
